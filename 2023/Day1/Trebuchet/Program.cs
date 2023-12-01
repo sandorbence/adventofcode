45a2ad9d860c20bc01445f1cebe308c2b8563145
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-12-01 09:46:30 +0100

    Add first half of first day

---
+++ 2023/Day1/Trebuchet/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Trebuchet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo baseDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+
+            string filePath = Path.Combine(baseDirectory.FullName, "input.txt");
+
+            string[] text = File.ReadAllLines(filePath);
+
+            List<int> digits = new List<int>();
+
+            foreach (string line in text)
+            {
+                digits.Add(FindAndConcatenateDigits(line));
+            }
+
+            Console.WriteLine(digits.Sum());
+        }
+
+        private static int FindAndConcatenateDigits(string line)
+        {
+            List<int> digits = new List<int>();
+
+            foreach (char c in line)
+            {
+                if (int.TryParse(c.ToString(), out int result))
+                    digits.Add(result);
+            }
+
+            return Convert.ToInt32($"{digits[0]}{digits[digits.Count - 1]}");
+        }
+    }
+}
