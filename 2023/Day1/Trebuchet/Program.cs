53590ad1862fcb2f3c6be90e04c3fb7f5799e76d
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-12-04 20:32:59 +0100

    Add first half of day 3

--- 2023/Day1/Trebuchet/Program.cs
+++ 2023/Day1/Trebuchet/Program.cs
@@ -10,17 +10,28 @@
 
             string[] text = File.ReadAllLines(filePath);
 
-            List<int> digits = new List<int>();
+            List<int> digitsFirst = new List<int>();
+            List<int> digitsSecond = new List<int>();
 
             foreach (string line in text)
             {
-                digits.Add(FindAndConcatenateDigits(line));
+                digitsFirst.Add(FindAndConcatenateDigits(line, true));
+                digitsSecond.Add(FindAndConcatenateDigits(line, false));
             }
 
-            Console.WriteLine(digits.Sum());
+            Console.WriteLine($"Sum for the first half: {digitsFirst.Sum()}");
+            Console.WriteLine($"Sum for the second half: {digitsSecond.Sum()}");
         }
 
-        private static int FindAndConcatenateDigits(string line)
+        private static int FindAndConcatenateDigits(string line, bool first)
+        {
+            if (first)
+                return FindAndConcatenateDigitsFirst(line);
+            else
+                return FindAndConcatenateDigitsSecond(line);
+        }
+
+        private static int FindAndConcatenateDigitsFirst(string line)
         {
             List<int> digits = new List<int>();
 
@@ -32,5 +43,30 @@
 
             return Convert.ToInt32($"{digits[0]}{digits[digits.Count - 1]}");
         }
+
+        private static int FindAndConcatenateDigitsSecond(string line)
+        {
+            string newLine = line;
+
+            foreach (var item in NumbersToReplace)
+            {
+                newLine = newLine.Replace(item.Key, item.Value);
+            }
+
+            return FindAndConcatenateDigitsFirst(newLine);
+        }
+
+        private static Dictionary<string, string> NumbersToReplace = new Dictionary<string, string>()
+        {
+            {"one", "o1e" },
+            {"two", "t2o" },
+            {"three", "th3ee" },
+            {"four", "fo4r" },
+            {"five", "fi5e" },
+            {"six", "s6x" },
+            {"seven", "se7en" },
+            {"eight", "ei8ht" },
+            {"nine", "ni9e" }
+        };
     }
 }
---
+++ 2023/Day3/GearRatios/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+
+namespace GearRatios
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo baseDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+
+            string filePath = Path.Combine(baseDirectory.FullName, "input.txt");
+
+            string[] text = File.ReadAllLines(filePath);
+
+            int partNumberSum = 0;
+
+            var firstRowNumbers = FindNumbers(text[0], out List<int> specialCharactersFirstRow);
+            FindNumbers(text[1], out List<int> specialCharactersSecondRow);
+            partNumberSum += FindPartNumbers(text[0], firstRowNumbers, specialCharactersFirstRow, indexesBefore: null, specialCharactersSecondRow);
+
+            for (int i = 1; i < text.Length - 1; i++)
+            {
+                var rowNumbers = FindNumbers(text[i], out List<int> specialCharacters);
+                FindNumbers(text[i - 1], out List<int> specialCharactersBefore);
+                FindNumbers(text[i + 1], out List<int> specialCharactersAfter);
+                partNumberSum += FindPartNumbers(text[i], rowNumbers, specialCharacters, specialCharactersBefore, specialCharactersAfter);
+            }
+
+            var lastRowNumbers = FindNumbers(text[text.Length - 1], out List<int> specialCharactersLastRow);
+            FindNumbers(text[text.Length - 2], out List<int> specialCharactersLastButOneRow);
+            partNumberSum += FindPartNumbers(text[text.Length - 1], lastRowNumbers, specialCharactersLastRow, specialCharactersLastButOneRow, indexesAfter: null);
+
+            Console.WriteLine(partNumberSum);
+        }
+
+        static List<(int index, int length, int value)> FindNumbers(string line, out List<int> specialCharacters)
+        {
+            List<(int index, int length, int value)> numbers = new List<(int, int, int)>();
+            specialCharacters = new List<int>();
+
+            for (int i = 0; i < line.Length;)
+            {
+                if (int.TryParse(line[i].ToString(), out _))
+                {
+                    string number = "";
+
+                    for (int j = i; j < line.Length; j++)
+                    {
+                        if (int.TryParse(line[j].ToString(), out _))
+                        {
+                            number += line[j];
+                        }
+                        else
+                            break;
+                    }
+
+                    numbers.Add((i, number.Length, Convert.ToInt32(number)));
+                    i += number.Length;
+                }
+                else if (line[i] != '.')
+                {
+                    specialCharacters.Add(i);
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return numbers;
+        }
+
+        static int FindPartNumbers(string line, List<(int index, int length, int value)> numbers, List<int> indexes, List<int> indexesBefore = null, List<int> indexesAfter = null)
+        {
+            int rowSum = 0;
+
+            foreach (var number in numbers)
+            {
+                bool isPartNumber = false;
+
+                // Check left
+                if (number.index > 0)
+                {
+                    // Check same row
+                    if (indexes.Contains(number.index - 1))
+                        isPartNumber = true;
+
+                    // Check row above
+                    if ((indexesBefore != null) && (indexesBefore.Contains(number.index - 1)))
+                        isPartNumber = true;
+
+                    // Check row below
+                    if ((indexesAfter != null) && (indexesAfter.Contains(number.index - 1)))
+                        isPartNumber = true;
+                }
+
+                int rightSide = number.index + number.length - 1;
+
+                // Check right
+                if (rightSide < line.Length - 1)
+                {
+                    // Check same row
+                    if (indexes.Contains(rightSide + 1))
+                        isPartNumber = true;
+
+                    // Check row above
+                    if ((indexesBefore != null) && (indexesBefore.Contains(rightSide + 1)))
+                        isPartNumber = true;
+
+                    // Check row below
+                    if ((indexesAfter != null) && (indexesAfter.Contains(rightSide + 1)))
+                        isPartNumber = true;
+                }
+
+                // Check top and bottom
+                for (int i = number.index; i < rightSide + 1; i++)
+                {
+                    if ((indexesBefore != null) && (indexesBefore.Contains(i)))
+                        isPartNumber = true;
+
+                    if ((indexesAfter != null) && (indexesAfter.Contains(i)))
+                        isPartNumber = true;
+                }
+
+                if (isPartNumber)
+                {
+                    rowSum += number.value;
+                }
+            }
+
+            return rowSum;
+        }
+
+    }
+}
