207bfa843d9a396f07cf3d7ed8f34c4bce9b75c6
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-12-05 16:09:18 +0100

    Add second part of day 3

--- 2023/Day3/GearRatios/Program.cs
+++ 2023/Day3/GearRatios/Program.cs
@@ -35,6 +35,12 @@ namespace GearRatios
             Console.WriteLine(partNumberSum);
         }
 
+        /// <summary>
+        /// Find all numbers and stopre their index in the row and their length and value.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="specialCharacters"></param>
+        /// <returns></returns>
         static List<(int index, int length, int value)> FindNumbers(string line, out List<int> specialCharacters)
         {
             List<(int index, int length, int value)> numbers = new List<(int, int, int)>();
@@ -73,6 +79,15 @@ namespace GearRatios
             return numbers;
         }
 
+        /// <summary>
+        /// Find all numbers that are next to a special character.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="numbers"></param>
+        /// <param name="indexes"></param>
+        /// <param name="indexesBefore"></param>
+        /// <param name="indexesAfter"></param>
+        /// <returns></returns>
         static int FindPartNumbers(string line, List<(int index, int length, int value)> numbers, List<int> indexes, List<int> indexesBefore = null, List<int> indexesAfter = null)
         {
             int rowSum = 0;
@@ -134,5 +149,113 @@ namespace GearRatios
             return rowSum;
         }
 
+        /// <summary>
+        /// Find all asterisks in a row.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        static List<int> FindAsterisks(string line)
+        {
+            List<int> indexes = new List<int>();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '*')
+                    indexes.Add(i);
+            }
+
+            return indexes;
+        }
+
+        /// <summary>
+        /// Find all numbers that are gears and return their gear ratio in one row.
+        /// </summary>
+        /// <param name="asteriskIndexes"></param>
+        /// <param name="rowLength"></param>
+        /// <param name="numbersSameRow"></param>
+        /// <param name="numbersBefore"></param>
+        /// <param name="numbersAfter"></param>
+        /// <returns></returns>
+        static List<int> FindGears(List<int> asteriskIndexes, int rowLength, List<(int index, int length, int value)> numbersSameRow, List<(int index, int length, int value)> numbersBefore = null, List<(int index, int length, int value)> numbersAfter = null)
+        {
+            List<int> gearRatios = new List<int>();
+
+            foreach (var index in asteriskIndexes)
+            {
+                List<int> gearValues = new List<int>();
+
+                List<(int index, int value)> rightSides = numbersSameRow.Select(number => (number.index + number.length - 1, number.value)).ToList();
+
+                if (numbersBefore != null)
+                    rightSides.AddRange(numbersBefore.Select(number => (number.index + number.length - 1, number.value)).ToList());
+
+                if (numbersAfter != null)
+                    rightSides.AddRange(numbersAfter.Select(number => (number.index + number.length - 1, number.value)).ToList());
+
+                // Check left
+                if ((index > 0) && (rightSides.Select(number => number.index).Contains(index - 1)))
+                {
+                    gearValues.Add(rightSides.Where(number => number.index == index - 1).ToList().First().value);
+                }
+
+                List<(int index, int value)> leftSides = numbersSameRow.Select(number => (number.index, number.value)).ToList();
+
+                if (numbersBefore != null)
+                    leftSides.AddRange(numbersBefore.Select(number => (number.index, number.value)).ToList());
+
+                if (numbersAfter != null)
+                    leftSides.AddRange(numbersAfter.Select(number => (number.index, number.value)).ToList());
+
+                // Check right
+                if ((index < rowLength - 1) && (leftSides.Select(number => number.index).Contains(index + 1)))
+                {
+                    gearValues.Add(leftSides.Where(number => number.index == index + 1).ToList().First().value);
+                }
+
+                List<(int index, int value)> numberLengths = new List<(int index, int value)>();
+
+                if (numbersBefore != null)
+                {
+                    foreach (var number in numbersBefore)
+                    {
+                        numberLengths.AddRange(ListIndexes(number.index, number.length, number.index));
+                    }
+                }
+
+                if (numbersAfter != null)
+                {
+                    foreach (var number in numbersAfter)
+                    {
+                        numberLengths.AddRange(ListIndexes(number.index, number.length, number.index));
+                    }
+                }
+
+                // Check top and bottom
+                if (numberLengths.Select(number => number.index).Contains(index))
+                {
+                    gearValues.Add(numberLengths.Where(number => number.index == index).First().value);
+                }
+
+                // Check if numbers make a gear
+                if (gearValues.Count == 2)
+                {
+                    gearRatios.Add(gearValues[0] * gearValues[1]);
+                }
+            }
+
+            return gearRatios;
+        }
+
+        static List<(int index, int value)> ListIndexes(int index, int length, int value)
+        {
+            List<(int index, int value)> indexes = new List<(int, int)>();
+
+            for (int i = index; i < index + length; i++)
+            {
+                indexes.Add((i, value));
+            }
+
+            return indexes;
+        }
     }
 }
