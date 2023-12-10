a9072505758470271fe86b868a6400e22854eab6
sandorbence <szaniszlo47@gmail.com>
2023-12-10 16:46:24 +0100

    Add first half of day 7

---
+++ 2023/Day7/CamelCards/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+
+namespace CamelCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo baseDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+
+            string filePath = Path.Combine(baseDirectory.FullName, "input.txt");
+
+            string[] text = File.ReadAllLines(filePath);
+
+            List<(string hand, int bet)> hands = new List<(string, int)>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                hands.Add(GetHandsAndBets(text[i]));
+            }
+
+            List<(string hand, int bet)> rankedHands = RankHands(hands);
+
+            int finalScore = 0;
+
+            for (int i = 0; i < rankedHands.Count; i++)
+            {
+                finalScore += (i + 1) * rankedHands[i].bet;
+            }
+
+            Console.WriteLine($"First half: {finalScore}");
+        }
+
+        /// <summary>
+        /// Get the hand and bet from an input line.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        static (string hand, int bet) GetHandsAndBets(string line)
+        {
+            return (line.Split(' ')[0], Convert.ToInt32(line.Split(' ')[1]));
+        }
+
+        /// <summary>
+        /// Get the type of a hand.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        static string GetType(string hand)
+        {
+            List<(char symbol, int count)> cards = new List<(char symbol, int count)>();
+
+            foreach (char c in hand)
+            {
+                if (!cards.Select(card => card.symbol).ToList().Contains(c))
+                {
+                    cards.Add((c, 1));
+                }
+                else
+                {
+                    var cardsWithSymbol = cards.Where(card => card.symbol == c).First();
+                    int index = cards.IndexOf(cardsWithSymbol);
+
+                    (char sym, int count) = (cardsWithSymbol.symbol, cardsWithSymbol.count + 1);
+
+                    cards[index] = (sym, count);
+                }
+            }
+
+            int jokerCount = cards.Any(card => card.symbol == 'J') ? cards.Where(card => card.symbol == 'J').First().count : 0;
+
+            int maxCount = cards.Select(card => card.count).Max();
+
+            return maxCount switch
+            {
+                5 => "Five",
+                4 => "Four",
+                3 => cards.Count switch
+                {
+                    2 => "FullHouse",
+                    _ => "Three"
+                },
+                2 => cards.Count switch
+                {
+                    3 => "TwoPairs",
+                    _ => "OnePair"
+                },
+                1 => "HighCard",
+                _ => "",
+            };
+        }
+
+        /// <summary>
+        /// Rank all hands by types and then by symbols.
+        /// </summary>
+        /// <param name="hands"></param>
+        /// <returns></returns>
+        static List<(string hand, int bet)> RankHands(List<(string hand, int bet)> hands)
+        {
+            List<(string hand, int bet, int typeRank)> sortedHands = new List<(string hand, int bet, int typeRank)>();
+
+            sortedHands.AddRange(hands.Select(hand => (hand.hand, hand.bet, typeRanks[GetType(hand.hand)])));
+
+            sortedHands = sortedHands.OrderBy(hand => hand.typeRank).ToList();
+
+            List<(string hand, int bet)> orderedHands = new List<(string hand, int bet)>();
+
+            foreach (var type in sortedHands.GroupBy(hand => hand.typeRank))
+            {
+                List<(string hand, int bet)> handsOfType = type.Select(hand => (hand.hand, hand.bet)).ToList();
+
+                orderedHands.AddRange(handsOfType.OrderBy(hand => hand.hand, new HandComparer()).ToList());
+            }
+
+            return orderedHands;
+        }
+
+        /// <summary>
+        /// Compare hands by symbols.
+        /// </summary>
+        class HandComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] == y[i])
+                        continue;
+                    if (symbolRanks[x[i]] > symbolRanks[y[i]])
+                        return 1;
+                    else if (symbolRanks[x[i]] < symbolRanks[y[i]])
+                        return -1;
+                }
+
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Ranking for types.
+        /// </summary>
+        static Dictionary<string, int> typeRanks = new Dictionary<string, int>()
+        {
+            { "HighCard", 0 },
+            { "OnePair", 1 },
+            { "TwoPairs", 2 },
+            { "Three", 3 },
+            { "FullHouse", 4 },
+            { "Four", 5 },
+            { "Five", 6 }
+        };
+
+        /// <summary>
+        /// Ranking for symbols.
+        /// </summary>
+        static Dictionary<char, int> symbolRanks = new Dictionary<char, int>()
+        {
+            { '2', 0 },
+            { '3', 1 },
+            { '4', 2 },
+            { '5', 3 },
+            { '6', 4 },
+            { '7', 5 },
+            { '8', 6 },
+            { '9', 7 },
+            { 'T', 8 },
+            { 'J', 9 },
+            { 'Q', 10 },
+            { 'K', 11 },
+            { 'A', 12 }
+        };
+    }
+}
