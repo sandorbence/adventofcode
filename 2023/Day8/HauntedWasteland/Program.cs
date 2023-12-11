8dd5a58cfeec76b437f016915fb09e182194e032
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-12-11 08:38:43 +0100

    Add first half of day 8

--- 2023/Day8/HauntedWasteland/Program.cs
+++ 2023/Day8/HauntedWasteland/Program.cs
@@ -12,9 +12,13 @@
 
 
             ParseInput(text);
+            int moves = Move();
+            Console.WriteLine($"Total number of moves needed: {moves}");
         }
 
-        static Dictionary<string, (string left, string right)> points = new Dictionary<string, (string left, string right)>();
+        static Dictionary<Point, (string left, string right)> points = new Dictionary<Point, (string left, string right)>();
+        static string instructions = "";
+        static Point currentPoint;
 
         /// <summary>
         /// Parse the input string.
@@ -23,14 +27,13 @@
         static void ParseInput(string[] text)
         {
             int startIndex = 0;
-            string instructions = "";
 
             // For multiple lines of instructions
             for (int i = 0; i < text.Length; i++)
             {
                 if (string.IsNullOrEmpty(text[i]))
                 {
-                    startIndex = i;
+                    startIndex = i + 1;
                     break;
                 }
                 instructions += text[i];
@@ -39,12 +42,18 @@
             for (int i = startIndex; i < text.Length; i++)
             {
                 string[] sides = text[i].Split('=');
-                string left = sides[1].Split(',')[0].Substring(1);
+                string left = sides[1].Split(',')[0].Trim().Substring(1);
                 string right = sides[1].Split(',')[1].Trim().Substring(0, 3);
 
-                points[sides[0].Trim()] = (left, right);
+                points[new Point(sides[0].Trim())] = (left, right);
             }
 
+            // Add left and right points to every point after every point has been instantiated
+            foreach (var point in points)
+            {
+                point.Key.Left = points.Where(item => item.Key.Name == point.Value.left).First().Key;
+                point.Key.Right = points.Where(item => item.Key.Name == point.Value.right).First().Key;
+            }
         }
 
         /// <summary>
@@ -52,18 +61,49 @@
         /// </summary>
         /// <param name="point"></param>
         /// <param name="instruction"></param>
-        static void MoveOne((string left, string right) point, string instruction)
+        static void MoveOne(Point point, char instruction)
         {
-
+            if (instruction == 'L')
+                currentPoint = point.Left;
+            else
+                currentPoint = point.Right;
         }
 
-        static void Move()
+        /// <summary>
+        /// Move from point to point until we reach ZZZ.
+        /// </summary>
+        /// <returns></returns>
+        static int Move()
         {
-            var startingPoint = points["AAA"];
+            currentPoint = points.Where(point => point.Key.Name == "AAA").First().Key;
+            int moves = 0;
 
-            foreach (var point in points)
+            for (int i = 0; ; i++)
             {
+                // Start from the beginning
+                if (i >= instructions.Length)
+                    i = 0;
+                moves++;
+                MoveOne(currentPoint, instructions[i]);
+                if (currentPoint == points.Where(point => point.Key.Name == "ZZZ").First().Key)
+                    break;
+            }
 
+            return moves;
+        }
+
+        /// <summary>
+        /// Class representing one point.
+        /// </summary>
+        public class Point
+        {
+            public string Name;
+            public Point Left { get; set; }
+            public Point Right { get; set; }
+
+            public Point(string name)
+            {
+                this.Name = name;
             }
         }
     }
