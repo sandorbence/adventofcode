ffa9b6a8600116491bb5ff0f5546aa133067068a
sandorbence <szaniszlo47@gmail.com>
2024-12-01 13:31:18 +0100

    Add day1 second half

--- 2024/Day1/HistorianHysteria/Program.cs
+++ 2024/Day1/HistorianHysteria/Program.cs
@@ -8,23 +8,29 @@ namespace HistorianHysteria
     {
         static void Main(string[] args)
         {
-            CalculateDiff();
+            string[] input = ParseInput();
+
+            CalculateDiff(input);
+            CalculateSimilarity(input);
         }
 
-        private static void CalculateDiff()
+        private static string[] ParseInput()
         {
             DirectoryInfo baseDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
 
             string filePath = Path.Combine(baseDirectory.FullName, "input.txt");
 
-            string[] text = File.ReadAllLines(filePath);
+            return File.ReadAllLines(filePath);
+        }
 
+        private static void CalculateDiff(string[] input)
+        {
             int diffSum = 0;
 
             List<int> leftNums = new List<int>();
             List<int> rightNums = new List<int>();
 
-            foreach (string line in text)
+            foreach (string line in input)
             {
                 int num1 = Convert.ToInt32(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
                 int num2 = Convert.ToInt32(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
@@ -41,7 +47,40 @@ namespace HistorianHysteria
                 diffSum += Math.Abs(leftNums[i] - rightNums[i]);
             }
 
-            Console.WriteLine(diffSum);
+            Console.WriteLine($"First half: {diffSum}");
+        }
+
+        private static void CalculateSimilarity(string[] input)
+        {
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+
+            int similarityScore = 0;
+
+            foreach (string line in input)
+            {
+                int num1 = Convert.ToInt32(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
+
+                if (!nums.ContainsKey(num1))
+                {
+                    nums[num1] = 1;
+                }
+                else
+                {
+                    nums[num1]++;
+                }
+            }
+
+            foreach (string line in input)
+            {
+                int num2 = Convert.ToInt32(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+
+                if (nums.ContainsKey(num2))
+                {
+                    similarityScore += nums[num2] * num2;
+                }
+            }
+
+            Console.WriteLine($"Second half: {similarityScore}");
         }
     }
 }
