e99d5d5cef34aa0b847d8843924b7c318479b723
sandorbence <szaniszlo47@gmail.com>
2024-12-03 17:56:59 +0100

    Add start of day3 2024

---
+++ 2024/Day3/Mull_ItOver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Mull_ItOver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = ParseInput();
+        }
+
+        private static string[] ParseInput()
+        {
+            DirectoryInfo baseDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            string filePath = Path.Combine(baseDirectory.FullName, "input.txt");
+
+            return File.ReadAllLines(filePath);
+        }
+
+        private static void FindMultiplications(string[] input)
+        {
+            foreach (string line in input)
+            {
+                string lineRest = line;
+
+                lineRest.IndexOf("mul(");
+            }
+        }
+    }
+}
