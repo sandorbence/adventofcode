ce6fd948d4313164fcfd57e102c65740cc0d8696
sandorbence <szaniszlo47@gmail.com>
2024-12-06 20:07:46 +0100

    Add second half of day 4 2024

--- 2024/Day4/CeresSearch/Program.cs
+++ 2024/Day4/CeresSearch/Program.cs
@@ -7,6 +7,7 @@
             string[] input = ParseInput();
 
             Console.WriteLine($"First half: {FindWords(input)}");
+            Console.WriteLine($"Second half: {FindXMas(input)}");
         }
         private static string[] ParseInput()
         {
@@ -65,11 +66,48 @@
             return count;
         }
 
+        private static int FindXMas(string[] input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (i < input.Length - 2 && j < input[i].Length - 2)
+                    {
+                        char middle = input[i + 1][j + 1];
+                        char topLeft = input[i][j];
+                        char bottomRight = input[i + 2][j + 2];
+                        char topRight = input[i + 2][j];
+                        char bottomLeft = input[i][j + 2];
+
+                        if (IsX_Mas(middle, topLeft, bottomRight, topRight, bottomLeft)) count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+
         private static bool IsXmas(char first, char second, char third)
         {
             if (first != 'M' || second != 'A' || third != 'S') return false;
 
             return true;
+        }
+
+        private static bool IsX_Mas(char middle, char topLeft, char bottomRight, char topRight, char bottomLeft)
+        {
+            if (middle != 'A') return false;
+
+            bool leftToRight = (topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M');
+            bool rightToLeft = (topRight == 'M' && bottomLeft == 'S') || (topRight == 'S' && bottomLeft == 'M');
+
+            if (leftToRight && rightToLeft) return true;
+
+            return false;
         }
     }
 }
