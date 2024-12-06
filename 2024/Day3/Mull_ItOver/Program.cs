8d9d10c036b7acd6e039161305aca455e3345774
sandorbence <szaniszlo47@gmail.com>
2024-12-06 16:45:55 +0100

    Add second part of day 3 2024

--- 2024/Day3/Mull_ItOver/Program.cs
+++ 2024/Day3/Mull_ItOver/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Mull_ItOver
@@ -10,6 +12,7 @@ namespace Mull_ItOver
         {
             string[] input = ParseInput();
             Console.WriteLine($"First half: {FindMultiplications(input)}");
+            Console.WriteLine($"Second half: {FindMultiplications(input, secondPart: true)}");
         }
 
         private static string[] ParseInput()
@@ -21,15 +24,25 @@ namespace Mull_ItOver
             return File.ReadAllLines(filePath);
         }
 
-        private static long FindMultiplications(string[] input)
+        private static long FindMultiplications(string[] input, bool secondPart = false)
         {
             long sum = 0;
 
             Regex multiplyings = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
             Regex numbers = new Regex(@"\d+");
 
+            Regex enable = new Regex(@"do\(\)");
+            Regex disable = new Regex(@"don't\(\)");
+
+            bool recentCommand = true;
+
             foreach (string line in input)
             {
+                Dictionary<int, bool> commands = enable.Matches(line).Union(
+                    disable.Matches(line)).ToDictionary(x => x.Index, x => x.Value == "do()");
+
+                commands[0] = recentCommand;
+
                 foreach (Match match in multiplyings.Matches(line))
                 {
                     int mult = 1;
@@ -39,8 +52,15 @@ namespace Mull_ItOver
                         mult *= Convert.ToInt32(num.Value);
                     }
 
-                    sum += mult;
+                    if (!secondPart ||
+                        (secondPart && commands[commands.Keys.Where(x => match.Index - x >= 0)
+                        .OrderByDescending(x => x).First()]))
+                    {
+                        sum += mult;
+                    }
                 }
+
+                recentCommand = commands.OrderByDescending(x => x.Key).First().Value;
             }
 
             return sum;
